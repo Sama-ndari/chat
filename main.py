cddf0691bea9ebd739c40da92f66c322868660db
Lacasse <Junior_julescesar10@hotmail.com>
2024-08-05 02:16:23 +0200

    first

--- main.py
+++ main.py
@@ -1,44 +1,51 @@
-from flask import Flask, render_template
-from flask_socketio import SocketIO
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
-from flask_cors import CORS
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///chat.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-socketio = SocketIO(app, cors_allowed_origins='*')
-CORS(app)
 db = SQLAlchemy(app)
+
 
 class Message(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     content = db.Column(db.String(200))
     timestamp = db.Column(db.DateTime, default=datetime.utcnow)
 
+
 with app.app_context():
     db.create_all()
 
-@app.route('/')
+
+@app.route('/', methods=['GET', 'POST'])
 def index():
+    # Fetch all the comments from the database
     messages = Message.query.all()
+
+    if request.method == 'POST':
+        # Get the comment from the form
+        comment = request.form['comment']
+        # Check if the comment already exists with the same submission time
+        existing_message = next(
+            (m for m in messages if m.content == comment and m.timestamp == request.form['timestamp']), None)
+
+        if existing_message:
+            return redirect(url_for('index'))
+
+        # Add the comment to the database
+        new_message = Message(content=comment)
+        db.session.add(new_message)
+        db.session.commit()
+
+        # Redirect the user back to the homepage
+        return redirect(url_for('index'))
+
+
+
+    # Render the template and pass the list of comments
     return render_template('index.html', messages=messages)
 
-@socketio.on('connect')
-def handle_connect():
-    print('Client connected')
-
-@socketio.on('disconnect')
-def handle_disconnect():
-    print('Client disconnected')
-
-@socketio.on('message')
-def handle_message(msg):
-    print(f'Received message: {msg}')
-    new_message = Message(content=msg)
-    db.session.add(new_message)
-    db.session.commit()
-    socketio.emit('message', msg)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True, allow_unsafe_werkzeug=True)
+    app.run(debug=True)
